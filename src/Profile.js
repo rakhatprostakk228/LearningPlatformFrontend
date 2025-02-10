d78e26158bc2a76bbc0303d78a81432fa31e3270
Rakhimbekov Rakhat <rkhantemir@mail.ru>
2025-02-10 22:17:01 +0500

    frontend

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Profile = ({ user }) => {
   const navigate = useNavigate();
